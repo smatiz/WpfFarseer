7d866a06979f491c8dbb3f61af6378c6b00f034e
mtiz <stephane.matiz@sms-concast.it>
2014-11-19 14:13:41 +0100

    commit flag

--- SM.Wpf/GroupControl.cs
+++ SM.Wpf/GroupControl.cs
@@ -11,26 +11,26 @@ using System.Windows.Media;
 namespace SM.Wpf
 {
     
-    [ContentPropertyAttribute("Children")]
-    public class GroupControl //: BasicControl
-    {
-        public GroupControl()
-        {
-            Children = new List<BasicControl>();
-            var views = XamlInterpreter.BuildViews(Children);
+    //[ContentPropertyAttribute("Children")]
+    //public class GroupControl //: BasicControl
+    //{
+    //    public GroupControl()
+    //    {
+    //        Children = new List<BasicControl>();
+    //        var views = XamlInterpreter.BuildViews(Children);
 
 
 
-        }
+    //    }
 
 
-        public List<BasicControl> Children
-        {
-            get { return (List<BasicControl>)GetValue(ChildrenProperty); }
-            set { SetValue(ChildrenProperty, value); }
-        }
-        public static readonly DependencyProperty ChildrenProperty =
-            DependencyProperty.Register("Children", typeof(List<BasicControl>), typeof(GroupControl), new PropertyMetadata(null));
+    //    public List<BasicControl> Children
+    //    {
+    //        get { return (List<BasicControl>)GetValue(ChildrenProperty); }
+    //        set { SetValue(ChildrenProperty, value); }
+    //    }
+    //    public static readonly DependencyProperty ChildrenProperty =
+    //        DependencyProperty.Register("Children", typeof(List<BasicControl>), typeof(GroupControl), new PropertyMetadata(null));
 
-    }
+    //}
 }
