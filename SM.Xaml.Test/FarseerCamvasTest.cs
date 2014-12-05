a9d23d1cd7af97c8bb9e5060e3f575a7319ed560
mtiz <stephane.matiz@sms-concast.it>
2014-12-05 13:35:08 +0100

    commit flag

---
+++ SM.Xaml.Test/FarseerCamvasTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace SM.Xaml.Test
+{
+
+    [ContentPropertyAttribute("FarseerContent")]
+    public class FarseerCanvasTest : Canvas
+    {
+        public SM.Xaml.BasicContainer FarseerContent
+        {
+            get { return (SM.Xaml.BasicContainer)GetValue(FarseerContentProperty); }
+            set { SetValue(FarseerContentProperty, value); }
+        }
+        public static readonly DependencyProperty FarseerContentProperty =
+            DependencyProperty.Register("FarseerContent", typeof(SM.Xaml.BasicContainer), typeof(FarseerCanvasTest), new PropertyMetadata(null));
+    }
+}
