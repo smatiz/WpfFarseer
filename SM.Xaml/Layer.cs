7d2407f537b9ab148122d2a0b9e73a751175aff4
mtiz <stephane.matiz@sms-concast.it>
2014-12-15 08:17:49 +0100

    commit flag

--- SM.Xaml/Layer.cs
+++ SM.Xaml/Layer.cs
@@ -16,8 +16,26 @@ namespace SM.Xaml
             set { SetValue(TransformProperty, value); }
         }
         public static readonly DependencyProperty TransformProperty =
-            DependencyProperty.Register("Transform", typeof(transform2d), typeof(Layer), new PropertyMetadata(transform2d.Null));
+            DependencyProperty.Register("Transform", typeof(transform2d), typeof(Layer), new FrameworkPropertyMetadata(transform2d.Null, FrameworkPropertyMetadataOptions.AffectsRender));
 
+
+
+        //public transform2d Transform
+        //{
+        //    get { return (transform2d)GetValue(TransformProperty); }
+        //    set { SetValue(TransformProperty, value); }
+        //}
+        //public static readonly DependencyProperty TransformProperty =
+        //    DependencyProperty.Register("Transform", typeof(transform2d), typeof(User),
+        //    new FrameworkPropertyMetadata(transform2d.Null, FrameworkPropertyMetadataOptions.AffectsRender, new PropertyChangedCallback(TransformPropertyChanged)));
+        //private static void TransformPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e) { ((User)obj).OnTransformChanged(); }
+        //private void OnTransformChanged()
+        //{
+        //    if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+        //    {
+        //        //User_Refresh_DesignTime();
+        //    }
+        //}
        
     }
 }
